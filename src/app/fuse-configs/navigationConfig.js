2edefdb4ac63d9ec7ce3a69cff93ba2efd310ab5
najiba chaoua <najiba.chaoua@vneuron.com>
2020-03-28 18:36:37 +0100

    init project

---
+++ src/app/fuse-configs/navigationConfig.js
@@ -0,0 +1,64 @@
+const navigationConfig = [
+    {
+        'id'      : 'applications',
+        'title'   : 'Applications',
+        'type'    : 'group',
+        'icon'    : 'apps',
+        'children': [
+            {
+                'id'   : 'example-component',
+                'title': 'Example',
+                'type' : 'item',
+                'icon' : 'whatshot',
+                'url'  : '/example'
+            },
+            {
+                'id'   : 'login-component',
+                'title': 'Login',
+                'type' : 'item',
+                'icon' : 'whatshot',
+                'url'  : '/login'
+            },
+            {
+                'id'   : 'register-component',
+                'title': 'register',
+                'type' : 'item',
+                'icon' : 'whatshot',
+                'url'  : '/register'
+            },
+            {
+                'id'   : 'dashbord-component',
+                'title': 'dashbord',
+                'type' : 'item',
+                'icon' : 'whatshot',
+                'url'  : '/'
+            },
+            {
+                'id'   : 'users-component',
+                'title': 'users',
+                'type' : 'item',
+                'icon' : 'whatshot',
+                'url'  : '/users'
+            },
+            {
+                'id'   : 'team-component',
+                'title': 'team',
+                'type' : 'item',
+                'icon' : 'whatshot',
+                'url'  : '/team'
+            },
+            {
+                'id'   : 'chat-component',
+                'title': 'chat',
+                'type' : 'item',
+                'icon' : 'whatshot',
+                'url'  : '/chat'
+            },
+            
+            
+            
+        ]
+    }
+];
+
+export default navigationConfig;
